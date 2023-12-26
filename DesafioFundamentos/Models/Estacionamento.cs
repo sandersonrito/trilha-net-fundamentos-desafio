7d6a215aaee506d2d0ff9316641042a3d0cc3e15
Sanderson Rito <sanderson_rito_15@hotmail.com>
2023-12-26 19:10:54 -0300

    Ao clonar o projeto do GitHub, notei uma disparidade de versões entre a usada no desafio e a instalada em minha máquina, exigindo a modificação para a versão 8.0 no arquivo csproj. Além disso, o desafio foi concluído!

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -12,36 +14,85 @@ namespace DesafioFundamentos.Models
             this.precoPorHora = precoPorHora;
         }
 
+        //Implementado!
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+
+            string placa = Console.ReadLine().ToUpper();
+            
+            try
+            {
+                if (!IsValidPlacaFormat(placa))
+                {
+                    throw new FormatException ("Placa inválida. Insira uma placa no formato correto (ABC1234 ou ABC1D23).");
+                }
+
+                else if (veiculos.Contains(placa))
+                {
+                    throw new InvalidOperationException ("Não é possível cadastrar este veículo, pois ele já está registrado. Por favor, insira uma placa válida.");
+                }
+
+                else
+                {
+                    veiculos.Add(placa);
+                    Console.WriteLine("Veículo estacionado com sucesso!");
+                }
+            }
+
+            //Erro de formatação
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            //Erro de operação
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
 
+        private bool IsValidPlacaFormat(string placa)
+        {
+              string pattern = @"^[A-Z]{3}\d{4}$|^[A-Z]{3}\d[A-Z]\d{2}$";
+              return Regex.IsMatch(placa, pattern);
+        }
+
+
+        //Implementado!
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine().ToUpper();;
 
-            // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            // Verifica se o veículo existe na lista e verifica se a condição está sendo atendida.
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper())) 
             {
+                int horas;
+
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
+                
+                // codigo usado para receber a qtdHoras digitada pelo usuário e armazenar o valor da string que foi convertida para um inteiro, onde será validado a conversão.
+                if(int.TryParse(Console.ReadLine(), out horas)) 
+                {
+                
+                decimal valorTotal;
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
-
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                valorTotal = precoInicial + precoPorHora * horas;
+                veiculos.Remove(placa); 
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                } 
+                
+                else
+                
+                {
+                    Console.WriteLine("As horas que você digitou está inválida. Por favor, insira um valor válido para as horas.");
+                }
+
             }
             else
             {
@@ -49,14 +100,22 @@ namespace DesafioFundamentos.Models
             }
         }
 
+        //Implementado!
         public void ListarVeiculos()
         {
             // Verifica se há veículos no estacionamento
             if (veiculos.Any())
             {
+                int count = 1;
+
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine($"Veiculo {count} - {veiculo}");
+                    count++;
+                }
+
             }
             else
             {
